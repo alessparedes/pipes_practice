ff36b3c92d267ab29a42ce977195429e0fb854b5
Alessandro Paredes <paredesandro@gmail.com>
2023-07-16 21:08:45 -0600

    Practice date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   arreglo   : number[] = [1,2,3,4,5,6,7,8,9,10,11,19];
   porcentaje: number = 0.234;
   salario   : number = 1234.5;
+  fecha     : Date = new Date();
 
   valorPromesa = new Promise<string>( (resolve) => {
     setTimeout(()=>{
