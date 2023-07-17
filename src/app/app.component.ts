3264738ffe34fc69fe9321d4128841ec1c9735f5
Alessandro Paredes <paredesandro@gmail.com>
2023-07-16 18:06:37 -0600

    Practice different pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes_practice';
+  nombre    : string = 'Christian';
+  PI        : number = Math.PI;
+  personajes: string[] = ['Ironman', 'Thor', 'Loki', 'Groot', 'Fury'];
+  arreglo   : number[] = [1,2,3,4,5,6,7,8,9,10,11,19];
+  porcentaje: number = 0.234;
+  salario   : number = 1234.5;
+
+  valorPromesa = new Promise<string>( (resolve) => {
+    setTimeout(()=>{
+      resolve('Llego la data')
+    },2000);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: '17 B',
+      casa: '19-18'
+    }
+  };
+  
 }
