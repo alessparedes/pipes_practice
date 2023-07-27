fce66b329505e925b1b9d9e28e9e8e1e2974b001
Alessandro Paredes <paredesandro@gmail.com>
2023-07-26 21:34:21 -0600

    Date pipe register other language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+import localES from '@angular/common/locales/es';
+import localFR from '@angular/common/locales/fr';
+
+registerLocaleData(localES);
+registerLocaleData(localFR);
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
