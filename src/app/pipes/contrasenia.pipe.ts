b48adee60c92a53e66af7cbf051deac1cddfcb4e
Alessandro Paredes <paredesandro@gmail.com>
2023-07-30 21:02:05 -0600

    password string custom pipe

--- src/app/pipes/contrasenia.pipe.ts
+++ src/app/pipes/contrasenia.pipe.ts
@@ -5,15 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ContraseniaPipe implements PipeTransform {
 
-  transform(value: string, activo: boolean): string {
-    let cadena = '';
-    if (activo) {
-      for (var i = 0; i<value.length; i++) {
-        cadena += '*';
-      }
-      return cadena;
-    }
-    return value;
+  transform(value: string, activo: boolean = true): string {
+    return (activo) ? '*'.repeat(value.length) : value;
   }
 
 }
